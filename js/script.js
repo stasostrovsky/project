e31cb8e171f92dc458fa936b59539280e2e596cd
Stas <stas_ostrovsky@hotmail.com>
2020-11-20 15:22:43 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//alert(1);
+//console.log(1);
+//let number = 5;
+//const leftBorderWidth = 1;
+
+//const obj = {
+ //   name: "Стас",
+ //   age: 54,
+ //   isMarried: true
+//};
+
+//console.log(obj);
+
+//let id = Symbol("id");
+//console.log(id.toString());
+//alert(id.toString());
+//alert("Hello");
+//const result = confirm("Вы здесь?");
+//console.log(result);
+
+/* const result = +prompt("Вы здесь?", "18");
+console.log(result+2);
+ */
+/* const answer = [];
+answer[0] = prompt('Фамилия',"");
+answer[1] = prompt('Имя',"");
+answer[2] = prompt('ФВозрастИО',"");
+document.write(answer); */
+
+//  const categorya = "toys";
+// console.log('https://someurl.com/' + categorya + '/5');
+// console */.log(`https://someurl.com/${categorya}/5`);
+
+const user1 = 'stas';
+alert(`hello ${user1}`);
