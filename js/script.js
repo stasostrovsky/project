8be25478a424f4e2e089f11279a2cec5f8d09481
Stas <stas_ostrovsky@hotmail.com>
2022-10-31 15:28:03 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
+"use strict";
 //alert(1);
-//console.log(1);
-//let number = 5;
-//const leftBorderWidth = 1;
+/* const res = confirm("Что делать");
+console.log(res); */
+const answer = prompt("сколько лет","55");
+console.log(answer);
+/* let number = 5;
+const leftBorderWidth = 1;
+console.log(number);
+
+let id = Symbol("123фф");
+console.log(id.description); // id
+
+console.log(typeof "foo");
+console.log(typeof 125);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+let name = "Ilya";
+
+console.log( `hello ${1}` ); // ?
+
+console.log( `hello ${"name"}` ); // ?
+
+console.log( `hello ${name}` ); // ? */
 
 //const obj = {
  //   name: "Стас",
@@ -14,7 +41,7 @@
 //let id = Symbol("id");
 //console.log(id.toString());
 //alert(id.toString());
-//alert("Hello");
+// alert("Hello");
 //const result = confirm("Вы здесь?");
 //console.log(result);
 
@@ -31,5 +58,63 @@ document.write(answer); */
 // console.log('https://someurl.com/' + categorya + '/5');
 // console */.log(`https://someurl.com/${categorya}/5`);
 
-const user1 = 'stas';
-alert(`hello ${user1}`);
+// const user1 = 'stas';
+// alert(`hello ${user1}`);
+/* let counter = 2;
+counter++;        // работает как counter = counter + 1, просто запись короче
+console.log( ++counter ); */
+/* let message;
+message = 'Hello!';
+
+alert(message); */
+
+/* let message = "Это";
+
+// повторение ключевого слова 'let' приводит к ошибке
+let message1 = "Другое"; // SyntaxError: 'message' has already been declared
+ */
+/* let let = 5; // нельзя назвать переменную "let", ошибка!
+let return = 5; // также нельзя назвать переменную "return", ошибка! */
+/* let name1 = "Стас";
+
+// Вставим переменную
+alert( `Привет, ${name1}!` ); // Привет, Иван!
+
+// Вставим выражение
+alert( `результат: ${1 + 2}` ); // результат: 3 */
+
+/* console.log( Number("   123   ") ); // 123
+console.log( Number("123z") );      // NaN (ошибка чтения числа на месте символа "z")
+console.log( Number(true) );        // 1
+console.log( Number(false) );       // 0
+console.log( Number(undefined) );    
+
+console.log( Boolean(1) ); // true
+console.log( Boolean(0) ); // false
+
+console.log( Boolean("Привет!") ); // true */
+/* // console.log( typeof('aa'/2) ); // false// 0
+ let xer = 'aa'/25;
+console.log( xer ); // false// 0
+console. log( typeof(xer) ); // false// 0 
+*/
+/* console.log( Number("   123   ") ); // 123
+console.log( Number("123z") );      // NaN (ошибка чтения числа на месте символа "z")
+console.log( Number(true) );        // 1
+console.log( Number(false) );       // 0
+console.log(typeof 10);
+
+
+let str = "123";
+console.log(typeof str); // string
+
+let num = Number(str); // становится числом 123
+
+console.log("typeof: "+ typeof num); // number
+console.log("значение: " +  num); // number */
+/* console.log(typeof null);
+console.log(typeof undefined); */
+const User = "Stas";
+alert(`Привет, ${User}`);
+
+console.log(4 + "5");
