d0c3a60ebb9766729b90a5bc25434314bf005264
Stas <stas_ostrovsky@hotmail.com>
2022-11-14 19:28:55 +0300

    Упражнение 14

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const shoppingMallData = {
+/* const shoppingMallData = {
     shops: [
         {
             width: 10,
@@ -64,4 +64,44 @@ function СalcBudget(TotalVolumeShoppingCenter, moneyPer1m3) {
    return  (TotalVolumeShoppingCenter * moneyPer1m3);
 }
 
-console.log(isBudgetEnough(shoppingMallData));
+console.log(isBudgetEnough(shoppingMallData)); */
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    
+    arr.sort((a,b) => a.localeCompare(b));
+
+    let remainder = arr.length % 9;
+    let arrRemainingStudents = arr.splice(arr.length - remainder, remainder);
+    let countRow = arr.length / 3;
+
+    let arrResult = [];
+    let start = 0;
+    let end = 3;
+    for (let i = 0; i < countRow; i++) {
+      
+        let a1 = arr.slice(start,end);
+        console.log(a1);
+
+        arrResult[i] = a1;
+
+        start = end;
+        end = end + 3;
+
+    }
+
+    let msg = '';
+
+    if (remainder == 0) {
+        msg = 'Оставшиеся студенты: -';
+    } else {
+        msg = 'Оставшиеся студенты: ' + arrRemainingStudents.join(", ");
+    }
+
+    arrResult[arrResult.length] = msg;
+
+    return arrResult;
+}
+
+console.log(sortStudentsByGroups(students));
